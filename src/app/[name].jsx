bef83b2856d15a09403cc9305415be4584f76c99
Muchael123 <mainamichael2849@gmail.com>
2024-02-12 20:09:15 +0300

    Expand instructions

--- src/app/[name].jsx
+++ src/app/[name].jsx
@@ -1,17 +1,18 @@
-import { Text,  StyleSheet } from "react-native";
+import { Text,  StyleSheet, ScrollView } from "react-native";
 import { Stack, useLocalSearchParams } from "expo-router";
 import exercises from '../../assets/data/exercises.json'
-import { ScrollView } from "react-native";
+import { useState } from "react";
 
 export default function ExerciseDetails(){
     const params = useLocalSearchParams();
     const exercise = exercises.find((item) => item.name === params.name)
+    const [instruction, SetInstructions] = useState(false)
 
     if(!exercise){
         return <Text>Exercise not found</Text>
     }
     return (
-        <ScrollView ContentContainerStyle={styles.Exercises}>
+        <ScrollView contentContainerStyle={styles.Exercises}>
           <Stack.Screen options={{title: exercise.name}}/>
             <Text style={styles.text1}>{exercise.name}</Text>
       <Text style={styles.text2}>
@@ -19,7 +20,8 @@ export default function ExerciseDetails(){
          |{" "} <Text style={styles.subValue}>{exercise.equipment}</Text>
        
       </Text>
-      <Text style={styles.instructions}>{exercise.instructions}</Text>
+      <Text style={styles.instructions} numberOfLines={instruction ? 0: 3}>{exercise.instructions}</Text>
+      <Text style={styles.More} onPress={() => SetInstructions((prev) => !prev)}>{instruction ? 'See Less': 'See More'}</Text>
         </ScrollView>
     )
 }
@@ -29,11 +31,15 @@ const styles = StyleSheet.create({
       backgroundColor: "#fff",
       padding: 10,
       borderRadius: 10,
-      
+      gap: 12,
     },
     instructions: {
       fontSize:16,
       lineHeight: 22
+    },
+    More:{
+      alignSelf: 'center',
+      color: 'gray'
     },
     subValue: {
         textTransform: 'capitalize',
